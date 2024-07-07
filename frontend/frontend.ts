c1596892f0c038455486303ce84e5fb31af51cbf
ConstAbz <46004286+constantLine@users.noreply.github.com>
2024-07-07 17:54:05 +0500

    Revert "	modified:   frontend/frontend.ts"

--- frontend/frontend.ts
+++ frontend/frontend.ts
@@ -1,36 +1,20 @@
 import * as express from "express";
 import * as path from "path";
-import * as http from "http";
 
 const app = express();
 app.set('views', path.join(__dirname, '/'));
 app.set('view engine', 'jade');
 
-// Прокси для взаимодействия с backend через UNIX сокет
-const backendProxy = (req, res) => {
-  const options = {
-    socketPath: '/var/run/backend.sock',
-    path: '/cat_get',
-    method: 'GET',
-  };
-
-  const proxyReq = http.request(options, (proxyRes) => {
-    proxyRes.pipe(res, { end: true });
-  });
-
-  proxyReq.on('error', (err) => {
-    console.error(err);
-    res.sendStatus(500);
-  });
-
-  proxyReq.end();
-};
+const port: number = 3000;
+const backendUrl = "http://backend:3001";
 
 app.get('/', async (req, res) => {
-  backendProxy(req, res);
+    const cat_data = await fetch(`${backendUrl}/cat_get`);
+    const data = await cat_data.text();
+
+    res.render('index.jade', { title: "KIT Frontend", cat_url: data });
 });
 
-app.listen('/var/run/frontend.sock', () => {
-  console.log('Frontend app listening on /var/run/frontend.sock');
-});
-
+app.listen(port, () => {
+    console.log(`Frontend app listening on port ${port}`)
+})

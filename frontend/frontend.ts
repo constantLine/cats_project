6e64a7752bf4ab6476f23b3656c800b7fcf72a69
ConstAbz <kabzalimov@mail.ru>
2024-07-08 12:43:24 +0300

    new file:   backend/Dockerfile 	new file:   backend/nginx.conf 	new file:   backend/start.sh 	new file:   docker-compose.yml 	new file:   frontend/Dockerfile 	modified:   frontend/frontend.ts 	new file:   frontend/nginx.conf 	new file:   frontend/start.sh

--- frontend/frontend.ts
+++ frontend/frontend.ts
@@ -1,15 +1,16 @@
 import * as express from "express";
 import * as path from "path";
+import fetch from "node-fetch";
 
 const app = express();
 app.set('views', path.join(__dirname, '/'));
 app.set('view engine', 'jade');
 
 const port: number = 3000;
-const backendUrl = "http://backend:3001";
+const backendUrl = "http://backend/api/cat_get";
 
 app.get('/', async (req, res) => {
-    const cat_data = await fetch(`${backendUrl}/cat_get`);
+    const cat_data = await fetch(backendUrl);
     const data = await cat_data.text();
 
     res.render('index.jade', { title: "KIT Frontend", cat_url: data });
@@ -17,4 +18,5 @@ app.get('/', async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Frontend app listening on port ${port}`)
-})
+});
+

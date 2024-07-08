8070be6786364d3827af040e28413fda1cc7fa85
ConstAbz <kabzalimov@mail.ru>
2024-07-08 18:22:45 +0300

    modified:   backend/Dockerfile 	modified:   backend/nginx.conf 	modified:   backend/requirements.txt 	modified:   backend/start.sh 	modified:   backend/uwsgi.ini

--- backend/start.sh
+++ backend/start.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-# Запускаем uWSGI с использованием конфигурации
+# Определяем сокет
+SOCKET_FILE=/tmp/backend.sock
+
+# Удаляем сокет, если он уже существует
+if [ -S "$SOCKET_FILE" ]; then
+    rm $SOCKET_FILE
+fi
+
+# Запускаем backend с использованием uWSGI
 uwsgi --ini /app/uwsgi.ini &
 
 # Ожидаем, пока backend начнет слушать на сокете
 while [ ! -S "$SOCKET_FILE" ]; do
     sleep 1
 done
+
+# Устанавливаем права доступа на сокет
+chmod 666 $SOCKET_FILE
 
 # Запускаем nginx
 nginx -g 'daemon off;'

d0f7ed1e9f2db2a3778e368376809ff789a30521
ConstAbz <kabzalimov@mail.ru>
2024-07-08 13:27:57 +0300

    modified:   backend/nginx.conf 	modified:   docker-compose.yml 	modified:   frontend/frontend.ts 	modified:   frontend/nginx.conf

--- frontend/frontend.ts
+++ frontend/frontend.ts
@@ -6,15 +6,16 @@ app.set('views', path.join(__dirname, '/'));
 app.set('view engine', 'jade');
 
 const port: number = 3000;
-const backendUrl = "http://backend/api/cat_get";
+const backendUrl = "http://backend/api/cat_get";  // Обращение к бэкенду через Nginx на порту 80
 
 app.get('/', async (req: Request, res: Response) => {
     try {
-        const fetch = (await import("node-fetch")).default; // Динамический импорт node-fetch
+        const fetch = (await import("node-fetch")).default;
         const cat_data = await fetch(backendUrl);
         const data = await cat_data.text();
         res.render('index.jade', { title: "KIT Frontend", cat_url: data });
     } catch (error) {
+        console.error('Error fetching data from backend:', error);
         res.status(500).send('Error fetching data from backend');
     }
 });
@@ -22,7 +23,3 @@ app.get('/', async (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`Frontend app listening on port ${port}`);
 });
-
-
-
-
